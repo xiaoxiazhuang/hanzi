e551d193b61bc01affdb484e1a436395f6a02035
Xiaoxia Zhuang <xiaoxiazhuang@gmail.com>
2013-06-14 12:46:38 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hanzi::Application.config.secret_token = '9bbe92b14c3d268d62d4465ffc4f8e9270e0307614815a02e3178ec9152b69370f762ccca3bde784bd2f0844d24aafadc7ee5503cf8c4f7facca9623ec4071ec'

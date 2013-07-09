84f09e67358e185d7848ce641b3de55b41b6ea1f
Xiaoxia Zhuang <xiaoxiazhuang@gmail.com>
2013-07-09 18:06:28 -0400

    created new div for sample_flashcard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,3 +35,17 @@ $(function() {
   // });
 
 } );
+
+  $(document).ready(function(){
+      resizeDiv();
+  });
+
+  window.onresize = function(event) {
+      resizeDiv();
+  };
+
+  function resizeDiv() {
+      vpw = $(window).width(); 
+      vph = $(window).height(); 
+      $('#somediv').css({'height': vph + 'px'});
+  };
